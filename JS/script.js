ddfc6cc9d653054c92ec726394aedc9d16e878d8
Your Name <dheerajlowanshi04@gmail.com>
2025-08-27 15:05:30 +0530

    change transition effects

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
-// heading animation start
+// about section start
 var tl2 = gsap.timeline({
   scrollTrigger: {
-    trigger: ".headingAnimation",
+    trigger: ".aboutEffect",
     scroller: "body",
     start: "top 95%",
     end: "top 85%",
@@ -9,61 +9,111 @@ var tl2 = gsap.timeline({
     //markers: true,
   },
 });
-tl2.from("#title", {
+tl2.from("#aboutTitle", {
   x: 100,
   opacity: 0,
   duration: 0.8,
 });
-tl2.from("#heading", {
+tl2.from("#aboutHeading", {
   x: -100,
   opacity: 0,
   duration: 0.8,
 });
+// about section end
 
-var about = gsap.timeline({
+
+// project section start
+var tl2 = gsap.timeline({
   scrollTrigger: {
-    trigger: ".headingAnimation",
+    trigger: ".projectEffect",
     scroller: "body",
-    start: "top 50%",
-    end: "top 40%",
+    start: "top 95%",
+    end: "top 85%",
     scrub: 2,
     //markers: true,
   },
 });
-about.from(
-  "#about_image",
-  {
-    y: 100,
-    opacity: 0,
-    duration: 0.8,
+tl2.from("#projectTitle", {
+  x: 100,
+  opacity: 0,
+  duration: 0.8,
+});
+tl2.from("#projectHeading", {
+  x: -100,
+  opacity: 0,
+  duration: 0.8,
+});
+// project section end
+
+// contact section start
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".contactEffect",
+    scroller: "body",
+    start: "top 95%",
+    end: "top 85%",
+    scrub: 2,
+    //markers: true,
   },
-  "admin"
-);
-about.from(
-  "#about_description",
-  {
-    y: 100,
-    opacity: 0,
-    duration: 0.8,
-  },
-  "admin"
-);
-about.from(
-  "#education",
-  {
-    x: 100,
-    opacity: 0,
-    duration: 0.8,
-  },
-  "admin"
-);
-about.from(
-  "#tech_stack",
-  {
-    x: 100,
-    opacity: 0,
-    duration: 0.8,
-  },
-  "admin"
-);
+});
+tl2.from("#contactTitle", {
+  x: 100,
+  opacity: 0,
+  duration: 0.8,
+});
+tl2.from("#contactHeading", {
+  x: -100,
+  opacity: 0,
+  duration: 0.8,
+});
+// contact section end
+
+
+
+// var about = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".headingAnimation",
+//     scroller: "body",
+//     start: "top 50%",
+//     end: "top 40%",
+//     scrub: 2,
+//     //markers: true,
+//   },
+// });
+// about.from(
+//   "#about_image",
+//   {
+//     y: 100,
+//     opacity: 0,
+//     duration: 0.8,
+//   },
+//   "admin"
+// );
+// about.from(
+//   "#about_description",
+//   {
+//     y: 100,
+//     opacity: 0,
+//     duration: 0.8,
+//   },
+//   "admin"
+// );
+// about.from(
+//   "#education",
+//   {
+//     x: 100,
+//     opacity: 0,
+//     duration: 0.8,
+//   },
+//   "admin"
+// );
+// about.from(
+//   "#tech_stack",
+//   {
+//     x: 100,
+//     opacity: 0,
+//     duration: 0.8,
+//   },
+//   "admin"
+// );
 // heading animation end
